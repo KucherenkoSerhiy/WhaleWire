3fefc1f2fb60c7cdc6d2cbaae9c4a49e1b83a39a
serhiy kucherenko <kucherenkoserhiy@gmail.com>
2026-01-04 20:54:37 +0100

    feature(application): add messaging abstractions

---
+++ WhaleWire.Application/Messaging/IMessageConsumer.cs
@@ -0,0 +1,6 @@
+﻿namespace WhaleWire.Application.Messaging;
+
+public interface IMessageConsumer<T> where T: class
+{
+    Task HandleAsync<T>(T message, CancellationToken token = default) where T: class;
+}
---
+++ WhaleWire.Application/Messaging/IMessagePublisher.cs
@@ -0,0 +1,6 @@
+﻿namespace WhaleWire.Application.Messaging;
+
+public interface IMessagePublisher
+{
+    Task PublishAsync<T>(T message, CancellationToken token = default) where T: class;
+}
