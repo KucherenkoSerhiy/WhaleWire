9113f2ce74a2008a467d20728cfefc727b24b828
serhiy kucherenko <kucherenkoserhiy@gmail.com>
2026-01-04 20:54:03 +0100

    feature(messages): add CanonicalEventReady message contract

---
+++ WhaleWire.Messages/CanonicalEventReady.cs
@@ -0,0 +1,12 @@
+﻿namespace WhaleWire.Messages;
+
+public sealed record CanonicalEventReady
+{
+    public required string EventId { get; init; }
+    public required string Chain { get; init; }
+    public required string Address { get; init; }
+    public required long Lt { get; init; }
+    public required string TxHash { get; init; }
+    public required string RawJson { get; init; }
+    public required DateTime OccurredAt { get; init; }
+}
