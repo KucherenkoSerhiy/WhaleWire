a338c2267371640d28f76f415f057a0e5b43de34
serhiy kucherenko <kucherenkoserhiy@gmail.com>
2026-01-31 22:55:42 +0100

    chore: remove Chainstack (replaced by TonCenter)

--- WhaleWire.Infrastructure.Ingestion/Configuration/ChainstackOptions.cs
+++
@@ -1,8 +0,0 @@
-namespace WhaleWire.Infrastructure.Ingestion.Configuration;
-
-public sealed class ChainstackOptions
-{
-    public const string SectionName = "Chainstack";
-
-    public required string ApiUrl { get; init; }
-}
